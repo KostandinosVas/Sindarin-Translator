1f9014b280a85cfc2f5f7ae8b16f92d7742e8f77
Kostandinos <topapi30@gmail.com>
2024-08-18 11:35:59 +0300

    fixed the title on desktop

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={bilbo.className}>
       <div className='flex flex-col justify-between h-[100vh]'>
       <div className='flex flex-col items-center h-[80vh] justify-around gap-y-4 xl:w-2/3 xl:mx-auto'>
-        <h1 className='text-[64px] text-center font-bold text-amber-200 '>Sindarin Translator</h1>
+        <h1 className='text-[64px] text-center font-bold text-amber-200 xl:absolute xl:left-0 xl:top-2'>Sindarin Translator</h1>
         <ElvishTranslator />
       </div>
     </div>
